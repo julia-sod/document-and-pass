beb2ad39b78a38ce8ff1cc43846b034521014138
yulia <sochnevaj@gmail.com>
2020-07-09 20:52:00 +0200

    Completed exercise

--- exercises/1-sum-all/index.js
+++ exercises/1-sum-all/index.js
@@ -1,8 +1,25 @@
 /**
  *
  */
-const sumAll = () => {
+const sumAll = (a, b) => {
+  if (a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  }
 
-}
+  if (a < b) {
+    firstNumber = a;
+    secondNumber = b;
+  } else {
+    firstNumber = b;
+    secondNumber = a;
+  }
 
-module.exports = sumAll
+  let sum = 0;
+  for (let i = firstNumber; i <= secondNumber; i++) {
+    sum += i;
+  }
+
+  return sum;
+};
+
+module.exports = sumAll;
