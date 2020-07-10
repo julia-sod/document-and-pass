5337eeded7b142e64cb1172043a07c059483ad34
yulia <sochnevaj@gmail.com>
2020-07-10 11:57:23 +0200

    Added comment

--- exercises/1-sum-all/index.js
+++ exercises/1-sum-all/index.js
@@ -1,6 +1,12 @@
-/**
- *
+/** 
+* sums all the numbers between 2 given integers, including them
+* throws "ERROR" if one of the output is not a number or not an integer number
+
+* @param {number} a 
+* @param {number} b 
+* returns {number} - sum
  */
+
 const sumAll = (a, b) => {
   if (a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number") {
     return "ERROR";
