706bcb28527936d06d9900d2eb84f442c19cffe4
yulia <sochnevaj@gmail.com>
2020-07-10 12:57:10 +0200

    Completed exercise

--- exercises/1-repeat-string/index.js
+++ exercises/1-repeat-string/index.js
@@ -1,8 +1,20 @@
 /**
- *
+ * repeats string the given amount of times
+ * throws "ERROR" if times is a negative number
+ * @param {string} str - the given string
+ * @param {number} times - the amount of times the string should be repeated
+ * @return {string} - repeated string
  */
-const repeatString = () => {
-
+const repeatString = (str, times) => {
+    if (times < 0) {
+        return "ERROR";
+    }
+    let repeatedString = '';
+    while (times > 0) {
+    repeatedString += str;
+    times--;
+    }
+    return repeatedString;
 }
 
 module.exports = repeatString
