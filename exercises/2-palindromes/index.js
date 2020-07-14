d2867103e8b4a5b45e9a298226f86982cb3fc51f
yulia <sochnevaj@gmail.com>
2020-07-14 16:13:49 +0200

    Completed the exercise

--- exercises/2-palindromes/index.js
+++ exercises/2-palindromes/index.js
@@ -1,8 +1,14 @@
 /**
+ * checks if a given string is a palindrome
+ * @param {string} str - initial string
+ * @returns {boolean} - true or false
  *
  */
-const palindromes = () => {
-
+const palindromes = (str) => {
+    const re = /[\W_]/g;
+    const lowRegStr = str.toLowerCase().replace(re, '');
+    const reverseStr = lowRegStr.split('').reverse().join('');
+    return reverseStr === lowRegStr;
 };
 
 module.exports = palindromes;
