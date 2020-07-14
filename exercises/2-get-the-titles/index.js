26cf4589345d2f48be102af3fb7a2a9a386add73
yulia <sochnevaj@gmail.com>
2020-07-14 17:09:30 +0200

    Completed the exercise

--- exercises/2-get-the-titles/index.js
+++ exercises/2-get-the-titles/index.js
@@ -1,8 +1,10 @@
 /**
+ * takes the array and returns an array of titles
+ * @param {array} arr - array of objects
+ * @returns {array} - array of titles
  *
  */
-const getTheTitles = () => {
+const getTheTitles = (arr) => arr.map(val => val.title) 
 
-}
 
 module.exports = getTheTitles;
