06bbf4c942c934ed2fe36b16567a25bf78431b68
yulia <sochnevaj@gmail.com>
2020-07-14 22:09:48 +0200

    Completed the exercise

--- exercises/2-fibonacci/index.js
+++ exercises/2-fibonacci/index.js
@@ -1,7 +1,17 @@
 /**
- *
+ * returns a specific member of the fibonacci sequence, the sum of the two preceding numbers
+ * @param {number} n - index of element from Fibonacci array
+ * @returns {number} - element with index n from Fibonacci array
  */
-const fibonacci = () => {
+const fibonacci = (n) => {
+    if (n < 0) {
+        return "OOPS";
+    }
+    let arr = [0, 1];
+    for (let i = 2; i <= n; i++){
+    arr.push(arr[i - 2] + arr[i -1]);
+  }
+ return arr[n];
 
 }
 
